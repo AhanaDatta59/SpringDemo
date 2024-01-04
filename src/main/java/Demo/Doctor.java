925975e23363ed61a52dd2f139bf32aa1932de7d
AhanaDatta59 <ahanadatta59@gmail.com>
2024-01-04 23:16:20 +0530

    Repo Intialized

--- src/main/java/Demo/Doctor.java
+++ src/main/java/Demo/Doctor.java
@@ -1,10 +1,10 @@
 package Demo;
 
-public class Doctor {
+public class Doctor implements Staff {
     public void assist() {
         System.out.println("Doctor is assisting");
     }
-//   public static void main(String[] args){
+
 //       assist();
 //   }
 }
--- src/main/java/Demo/Main.java
+++ src/main/java/Demo/Main.java
@@ -1,7 +1,23 @@
 package Demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor();
-        doctor.assist();
+
+        //we are using XML based configuration only
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("spring.xml");
+
+//        //way 1
+//        Doctor doctor = context.getBean(Doctor.class);
+//
+//        //way2
+//        Nurse nurse =(Nurse) context.getBean("nurse");
+//        doctor.assist();
+//        nurse.assist();
+        Staff staff = context.getBean(Doctor.class);
+        staff.assist();
     }
 }
