c3a9b5c7bc29e03aecac8b10ead51b01f42ed18e
AhanaDatta59 <ahanadatta59@gmail.com>
2024-01-04 19:24:06 +0530

    Repo Intialized

---
+++ src/main/java/Demo/Doctor.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Doctor {
+    public void assist() {
+        System.out.println("Doctor is assisting");
+    }
+//   public static void main(String[] args){
+//       assist();
+//   }
+}
